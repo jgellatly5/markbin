9d8a731e129186dbd03c351cda19aa31d9d64d62
jgellatly5 <jgellatly5@gmail.com>
2017-02-12 11:36:57 -0800

    remove bin functionality
    remove bin functionality

--- client/components/bins/bins_list.js
+++ client/components/bins/bins_list.js
@@ -4,11 +4,21 @@ import { Bins } from '../../../imports/collections/bins';
 import { Meteor } from 'meteor/meteor';
 
 class BinsList extends Component {
+	onBinRemove(bin) {
+		Meteor.call('bins.remove', bin);
+	}
 	renderList() {
 		return this.props.bins.map(bin => {
 			return (
 				<li className="list-group-item" key={bin._id}>
 					Bin {bin._id}
+					<span className="pull-right">
+						<button
+							className="btn btn-danger"
+							onClick={() => this.onBinRemove(bin)}>
+							Remove
+						</button>
+					</span>
 				</li>
 			);
 		});
